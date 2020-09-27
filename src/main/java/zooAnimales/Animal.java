f41c779b83ce9d56c2e743edcb2bd6fc8e37cfe9
JuanPablo gt <69688315+JuanPabloGutierrezTamayo@users.noreply.github.com>
2020-09-27 17:19:12 -0500

    correció getters and setters

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -33,11 +33,11 @@ public class Animal {
 	}
 	
 	public void totalPorTipo() {
-		System.out.println("Mamiferos : "+ Mamifero.cantidadMamiferos());   
-		System.out.println("Aves : "+ Ave.cantidadAves());
-		System.out.println("Reptiles : "+ Reptil.cantidadReptiles());
-		System.out.println("Peces : "+Pez.cantidadPeces());
-		System.out.println("Anfibios : "+Anfibio.cantaidadAnfibios());
+		System.out.println("Mamiferos: "+ Mamifero.cantidadMamiferos());   
+		System.out.println("Aves: "+ Ave.cantidadAves());
+		System.out.println("Reptiles: "+ Reptil.cantidadReptiles());
+		System.out.println("Peces: "+Pez.cantidadPeces());
+		System.out.println("Anfibios: "+Anfibio.cantaidadAnfibios());
 		
 	}
 	public String movimiento() {
@@ -55,5 +55,36 @@ public class Animal {
 		}
 		
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public Zona getZona() {
+		return zona;
+	}
+	public void setZona(Zona zona) {
+		this.zona = zona;
+	}
+	
 	
 }
