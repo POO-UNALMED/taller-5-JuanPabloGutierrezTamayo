fc6ff62cf5cdce2b4ddf0bab306bc7c949b401f7
JuanPablo gt <69688315+JuanPabloGutierrezTamayo@users.noreply.github.com>
2020-09-27 19:43:36 -0500

    tostring corregido

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -43,11 +43,11 @@ public class Animal {
 	}
 	public String toString() {
 		if (this.zona==null) {
-			return "Mi nombres es "+this.nombre+", tengo una edad de "+this.edad+", habito en "+this.habitat+
+			return "Mi nombre es "+this.nombre+", tengo una edad de "+this.edad+", habito en "+this.habitat+
 					" y mi genero es "+this.genero;
 		}
 		else {
-			return "Mi nombres es "+this.nombre+", tengo una edad de "+this.edad+", habito en "+this.habitat+
+			return "Mi nombre es "+this.nombre+", tengo una edad de "+this.edad+", habito en "+this.habitat+
 					" y mi genero es "+this.genero+ ", la zona en la que me ubico es "+ this.zona.getNombre()+
 					", en el zoo "+ this.zona.nombreZoo();
 		}
